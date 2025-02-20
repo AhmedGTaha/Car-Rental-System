57be6958761940c36a941fbdd7cb54cd01fa4505
Ahmed Taha <ahmedtahaghodhbani@gmail.com>
2025-02-20 18:41:16 +0000

    better footer

--- footer.php
+++ footer.php
@@ -1,5 +1,20 @@
-<footer class="footer mt-auto py-3 bg-dark text-white">
-    <div class="container text-center">
-        <p class="mb-1">&copy; <?php echo date("Y"); ?> Car Rental System. All Rights Reserved.</p>
-    </div>
-</footer>
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <footer class="footer mt-auto py-3 text-white" style="background-color: #0f1016; color: #f0f0f0;">
+        <div class="container text-center">
+            <p class="mb-1">&copy; <?php echo date("Y"); ?> Car Rental System. All Rights Reserved.</p>
+        </div>
+    </footer>
+</body>
+
+</html>
